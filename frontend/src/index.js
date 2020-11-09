f5e6424dcf7bf881d7da5246456409ffe66d96de
Mervin Christian T. Itorma <40752994+iMervinC@users.noreply.github.com>
2020-11-09 09:31:05 +0800

    product api integration to frintend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 import './bootstrap.min.css'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 
 serviceWorker.unregister()
