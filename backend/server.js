6b96934b9b19631b535d3bbbd97a12776da884f1
Mervin Christian T. Itorma <40752994+iMervinC@users.noreply.github.com>
2021-05-30 13:43:48 +0800

    CORS origin

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
@@ -13,6 +14,7 @@ connectDB()
 const app = express()
 
 app.use(express.json())
+app.use(cors({ origin: '*' }))
 
 app.get('/', (req, res) => {
   res.send('API is running..........')
